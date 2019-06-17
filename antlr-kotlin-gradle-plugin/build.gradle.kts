73579c7261a7dc1e29ea12d552bdc1192c8d4d1e
Federico Tomassetti <federico@tomassetti.me>
2019-06-17 17:49:37 +0200

    Version 0.0.4

--- antlr-kotlin-gradle-plugin/build.gradle.kts
+++ antlr-kotlin-gradle-plugin/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `java-gradle-plugin`
     `java-library`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "0.10.0"
 }
 
 dependencies {
@@ -10,10 +11,18 @@ dependencies {
     compile(project(":antlr-kotlin-target"))
 }
 
+pluginBundle {
+    website = "https://github.com/Strumenta/antlr-kotlin" 
+    vcsUrl = "https://github.com/Strumenta/antlr-kotlin.git" 
+    tags = listOf("antlr", "kotlin", "parsing") 
+}
+
 gradlePlugin {
     plugins {
         create("antlr-kotlin") {
             id = "com.strumenta.antlr-kotlin"
+            displayName = "ANTLR Kotlin"
+            description = "Kotlin target for ANTLR. Generate multi-platform parsers in Kotlin"
             implementationClass = "com.strumenta.antlrkotlin.gradleplugin.AntlrKotlinPlugin"
         }
     }
