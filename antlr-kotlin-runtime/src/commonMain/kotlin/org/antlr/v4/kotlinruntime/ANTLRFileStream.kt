f510b987349d250c3bd8783a92bcc38f7657562f
phyrian <pederi.gergo@gmail.com>
2019-09-09 12:54:15 +0200

    Removed empty lines

--- antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/ANTLRFileStream.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/ANTLRFileStream.kt
@@ -12,10 +12,8 @@ import org.antlr.v4.kotlinruntime.misc.Utils
 /**
  * This is an [ANTLRInputStream] that is loaded from a file all at once
  * when you construct the object.
- *
  */
 @Deprecated("as of 4.7 Please use {@link CharStreams} interface.")
-
 class ANTLRFileStream
 private constructor(override val sourceName: String, val charset: Charset = UTF8)
     : ANTLRInputStream() {
