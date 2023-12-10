5474dab9c661b7e4067a8d42685e7c0d52f43d01
Federico Tomassetti <federico@tomassetti.me>
2023-12-10 16:58:27 +0100

    moving dokka version to toml file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,5 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-pluginManagement {
-  val dokkaVersion: String by settings
-
-  repositories {
-    mavenCentral()
-    gradlePluginPortal()
-  }
-
-  plugins {
-    id("org.jetbrains.dokka") version dokkaVersion
-  }
-}
-
 rootProject.name = "antlr-kotlin"
 
 include("antlr-kotlin-runtime")
