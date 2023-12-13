d8db5faf0c3fc82d4229c2904eaa061a25a0b99a
Edoardo Luppi <lp.edoardo@gmail.com>
2023-12-14 00:52:04 +0100

    refactor: rename OrderedHashSet.elements with JsName to avoid a name clash

--- antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/misc/OrderedHashSet.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/misc/OrderedHashSet.kt
@@ -6,6 +6,8 @@
 
 package org.antlr.v4.kotlinruntime.misc
 
+import kotlin.js.JsName
+
 /**
  * A HashMap that remembers the order that the elements were added.
  * You can alter the ith element with set(i,value) too :)  Unique list.
@@ -19,28 +21,27 @@ public open class OrderedHashSet<T> private constructor(
 
   /**
    * Track the elements as they are added to the set.
-   *
-   * Note(Edoardo): renamed from "elements" to "myElements" to avoid a JS name clash
    */
-  protected var myElements: ArrayList<T> = ArrayList()
+  @JsName("myElements")
+  protected var elements: ArrayList<T> = ArrayList()
 
   public fun get(i: Int): T =
-    myElements[i]
+    elements[i]
 
   /**
    * Replace an existing value with a new value; updates the element
    * list and the hash table, but not the key as that has not changed.
    */
   public fun set(i: Int, value: T): T {
-    val oldElement = myElements[i]
-    myElements[i] = value         // Update list
+    val oldElement = elements[i]
+    elements[i] = value         // Update list
     delegate.remove(oldElement)   // Now update the set: remove/add
     delegate.add(value)
     return oldElement
   }
 
   public fun remove(i: Int): Boolean {
-    val o = myElements.removeAt(i)
+    val o = elements.removeAt(i)
     return delegate.remove(o)
   }
 
@@ -53,7 +54,7 @@ public open class OrderedHashSet<T> private constructor(
 
     if (result) {
       // Only track if new element not in set
-      myElements.add(element)
+      elements.add(element)
     }
 
     return result
@@ -63,26 +64,26 @@ public open class OrderedHashSet<T> private constructor(
     throw UnsupportedOperationException()
 
   public override fun clear() {
-    myElements.clear()
+    elements.clear()
     delegate.clear()
   }
 
   public override fun hashCode(): Int =
-    myElements.hashCode()
+    elements.hashCode()
 
   public override fun equals(other: Any?): Boolean =
-    other is OrderedHashSet<*> && myElements == other.myElements
+    other is OrderedHashSet<*> && elements == other.elements
 
   public override fun iterator(): MutableIterator<T> =
-    myElements.iterator()
+    elements.iterator()
 
   /**
    * Return the List holding list of table elements.
    * Note that you are NOT getting a copy so don't write to the list.
    */
   public fun elements(): List<T> =
-    myElements
+    elements
 
   public override fun toString(): String =
-    myElements.toString()
+    elements.toString()
 }
