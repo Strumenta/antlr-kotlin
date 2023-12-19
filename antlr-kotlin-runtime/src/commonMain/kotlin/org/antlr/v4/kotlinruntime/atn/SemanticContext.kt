35d233369eddee74230c87a60d1ea62e8db1e80a
Edoardo Luppi <lp.edoardo@gmail.com>
2023-12-19 09:41:54 +0100

    refactor: cleanup SemanticContext

--- antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/atn/SemanticContext.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/atn/SemanticContext.kt
@@ -15,7 +15,6 @@ import org.antlr.v4.kotlinruntime.atn.SemanticContext.*
 import org.antlr.v4.kotlinruntime.misc.MurmurHash
 import kotlin.jvm.JvmStatic
 
-
 /**
  * A tree structure used to record the semantic context in which
  * an ATN configuration is valid.
@@ -70,23 +69,19 @@ public abstract class SemanticContext {
     }
 
     private fun filterPrecedencePredicates(collection: MutableCollection<out SemanticContext>): List<PrecedencePredicate> {
-      var result: ArrayList<PrecedencePredicate>? = null
+      val result = ArrayList<PrecedencePredicate>()
       val iterator = collection.iterator()
 
       while (iterator.hasNext()) {
         val context = iterator.next()
 
         if (context is PrecedencePredicate) {
-          if (result == null) {
-            result = ArrayList()
-          }
-
           result.add(context)
           iterator.remove()
         }
       }
 
-      return result ?: emptyList()
+      return result
     }
   }
 
@@ -223,7 +218,7 @@ public abstract class SemanticContext {
         return true
       }
 
-      return this.precedence == other.precedence
+      return precedence == other.precedence
     }
 
     override fun toString(): String =
@@ -351,7 +346,7 @@ public abstract class SemanticContext {
     }
 
     override fun toString(): String =
-      opnds.asList().joinToString("&&")
+      opnds.joinToString("&&")
   }
 
   /**
@@ -458,6 +453,6 @@ public abstract class SemanticContext {
     }
 
     override fun toString(): String =
-      opnds.asList().joinToString("||")
+      opnds.joinToString("||")
   }
 }
