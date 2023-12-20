590fd8d0f03263ca156c7eb1e21607727eddcd1f
Edoardo Luppi <lp.edoardo@gmail.com>
2023-12-20 11:26:28 +0100

    refactor: do not create a function each time assert is called

--- antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/kotlinmultiplatform/Assert.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/kotlinmultiplatform/Assert.kt
@@ -4,13 +4,12 @@ import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
 @OptIn(ExperimentalContracts::class)
-public inline fun assert(condition: Boolean, lazyMessage: () -> String? = { null }) {
+public inline fun assert(condition: Boolean, message: String? = null) {
   contract {
     returns() implies condition
   }
 
   if (!condition) {
-    val message = lazyMessage()
     throw AssertionError(message)
   }
 }
