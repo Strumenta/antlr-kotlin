e09f3eb560b09434409e6f03892a8bd061b2bd8a
Edoardo Luppi <lp.edoardo@gmail.com>
2023-12-24 19:05:08 +0100

    feat: support reading environment variables in Node.js

--- antlr-kotlin-runtime/src/jsAndWasmSharedMain/kotlin/com/strumenta/antlrkotlin/runtime/Environment.kt
+++
@@ -1,9 +0,0 @@
-package com.strumenta.antlrkotlin.runtime
-
-@Suppress("NOTHING_TO_INLINE")
-internal actual inline fun platformGetEnv(name: String): String? {
-  // TODO(Edoardo): add support for Node.js
-  @Suppress("SpellCheckingInspection")
-  System.out.println("getenv: not yet supported for Node.js and browser")
-  return null
-}
--- antlr-kotlin-runtime/src/jsAndWasmSharedMain/kotlin/com/strumenta/antlrkotlin/runtime/IsNode.kt
+++
@@ -1,8 +0,0 @@
-package com.strumenta.antlrkotlin.runtime
-
-/**
- * Returns whether we are running in Node.js or not.
- *
- * @author Edoardo Luppi
- */
-internal expect val isNode: Boolean
