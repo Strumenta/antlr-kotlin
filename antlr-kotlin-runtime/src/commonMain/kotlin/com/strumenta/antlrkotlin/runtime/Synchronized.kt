fce58c6e7903e7960563d5336e17c145ef94302f
ftomassetti <federico@strumenta.com>
2024-01-16 15:05:48 +0100

    Amend license header in all source files

--- antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/Synchronized.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/Synchronized.kt
@@ -1,3 +1,5 @@
+// Copyright 2017-2024 Strumenta and contributors, licensed under Apache 2.0.
+// Copyright 2024 Strumenta and contributors, licensed under BSD 3-Clause.
 package com.strumenta.antlrkotlin.runtime
 
 // TODO(Edoardo): wait for AtomicFU 0.24.0, so that we can
--- antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/tree/xpath/XPathLexer.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/org/antlr/v4/kotlinruntime/tree/xpath/XPathLexer.kt
@@ -1,3 +1,5 @@
+// Copyright 2017-2024 Strumenta and contributors, licensed under Apache 2.0.
+// Copyright 2024 Strumenta and contributors, licensed under BSD 3-Clause.
 //
 // This lexer is generated from antlr-kotlin-tests/antlr/XPathLexer.g4 and formatted manually.
 //
