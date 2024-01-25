b88caa4af52d61e2e0003d65e30faed029ac8872
Edoardo Luppi <lp.edoardo@gmail.com>
2024-01-25 19:51:05 +0100

    refactor: explicitly override interface methods in expect declarations
    This is in preparation for K2.

--- antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/CopyOnWriteArrayList.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/CopyOnWriteArrayList.kt
@@ -5,4 +5,26 @@ package com.strumenta.antlrkotlin.runtime
 public expect class CopyOnWriteArrayList<E>() : MutableList<E> {
   // Convenience constructor to avoid initializing with mutable state
   public constructor(elements: Collection<E>)
+
+  override val size: Int
+  override fun contains(element: E): Boolean
+  override fun containsAll(elements: Collection<E>): Boolean
+  override fun get(index: Int): E
+  override fun indexOf(element: E): Int
+  override fun isEmpty(): Boolean
+  override fun iterator(): MutableIterator<E>
+  override fun lastIndexOf(element: E): Int
+  override fun add(element: E): Boolean
+  override fun add(index: Int, element: E)
+  override fun addAll(index: Int, elements: Collection<E>): Boolean
+  override fun addAll(elements: Collection<E>): Boolean
+  override fun clear()
+  override fun listIterator(): MutableListIterator<E>
+  override fun listIterator(index: Int): MutableListIterator<E>
+  override fun remove(element: E): Boolean
+  override fun removeAll(elements: Collection<E>): Boolean
+  override fun removeAt(index: Int): E
+  override fun retainAll(elements: Collection<E>): Boolean
+  override fun set(index: Int, element: E): E
+  override fun subList(fromIndex: Int, toIndex: Int): MutableList<E>
 }
