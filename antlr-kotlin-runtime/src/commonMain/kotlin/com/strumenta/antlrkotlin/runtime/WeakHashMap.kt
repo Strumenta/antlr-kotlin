bd6b7f8402a5d027e614cd77118c48b762423fb4
Edoardo Luppi <lp.edoardo@gmail.com>
2024-01-25 19:51:05 +0100

    refactor: explicitly override interface methods in expect declarations
    This is in preparation for K2.

--- antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/WeakHashMap.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/WeakHashMap.kt
@@ -2,4 +2,17 @@
 // Copyright 2024-present Strumenta and contributors, licensed under BSD 3-Clause.
 package com.strumenta.antlrkotlin.runtime
 
-public expect class WeakHashMap<K, V>() : MutableMap<K, V>
+public expect class WeakHashMap<K, V>() : MutableMap<K, V> {
+  override val size: Int
+  override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
+  override val keys: MutableSet<K>
+  override val values: MutableCollection<V>
+  override fun containsKey(key: K): Boolean
+  override fun containsValue(value: V): Boolean
+  override fun get(key: K): V?
+  override fun isEmpty(): Boolean
+  override fun clear()
+  override fun put(key: K, value: V): V?
+  override fun putAll(from: Map<out K, V>)
+  override fun remove(key: K): V?
+}
