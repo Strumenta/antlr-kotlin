303d2aab291bdbb891fc83e7811e810983c5a05b
Edoardo Luppi <lp.edoardo@gmail.com>
2024-05-14 11:35:43 +0200

    build: remove the Canary version of Node.js
    Kotlin 1.9.24 uses Node.js 22 by default.

--- buildSrc/src/main/kotlin/com/strumenta/antlrkotlin/gradle/plugins/StrumentaMultiplatformModulePlugin.kt
+++ buildSrc/src/main/kotlin/com/strumenta/antlrkotlin/gradle/plugins/StrumentaMultiplatformModulePlugin.kt
@@ -4,14 +4,10 @@ import com.strumenta.antlrkotlin.gradle.ext.kmpExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
 import org.jetbrains.kotlin.gradle.targets.js.yarn.yarn
 
 /**
@@ -44,23 +40,6 @@ class StrumentaMultiplatformModulePlugin : Plugin<Project> {
       apiVersion.set(KotlinVersion.KOTLIN_1_9)
       languageVersion.set(KotlinVersion.KOTLIN_1_9)
       freeCompilerArgs.add("-Xexpect-actual-classes")
-    }
-
-    // The following is required to support the wasmJs target.
-    //
-    // Node.js Canary is set to 21.0.0-v8-canary20231019bd785be450
-    // as that is the last version to ship Windows binaries too.
-    project.rootProject.extensions.configure<NodeJsRootExtension> {
-      nodeVersion = "21.0.0-v8-canary20231019bd785be450"
-      nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-    }
-
-    project.rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
-      val flag = "--ignore-engines"
-
-      if (!args.contains(flag)) {
-        args.add(flag)
-      }
     }
   }
 }
