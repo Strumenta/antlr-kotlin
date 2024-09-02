0a9faf73c09b217bd44d895365804f95d35e09ed
Edoardo Luppi <lp.edoardo@gmail.com>
2024-09-02 14:58:34 +0200

    refactor: make Collections internal

--- antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/Collections.kt
+++ antlr-kotlin-runtime/src/commonMain/kotlin/com/strumenta/antlrkotlin/runtime/Collections.kt
@@ -2,10 +2,10 @@
 // Copyright 2024-present Strumenta and contributors, licensed under BSD 3-Clause.
 package com.strumenta.antlrkotlin.runtime
 
-public object Collections {
-  public fun <T : Comparable<T>> min(collection: Collection<T>): T =
+internal object Collections {
+  fun <T : Comparable<T>> min(collection: Collection<T>): T =
     collection.minOrNull() ?: throw NoSuchElementException()
 
-  public fun <T : Comparable<T>> max(collection: Collection<T>): T =
+  fun <T : Comparable<T>> max(collection: Collection<T>): T =
     collection.maxOrNull() ?: throw NoSuchElementException()
 }
