e239f150ff32d48c01404a40fd86c7aa9d9e3467
Edoardo Luppi <lp.edoardo@gmail.com>
2024-09-02 15:06:12 +0200

    refactor: make WeakHashMap internal

--- antlr-kotlin-runtime/src/nativeMain/kotlin/com/strumenta/antlrkotlin/runtime/WeakHashMap.kt
+++ antlr-kotlin-runtime/src/nativeMain/kotlin/com/strumenta/antlrkotlin/runtime/WeakHashMap.kt
@@ -1,9 +1,9 @@
 // Copyright 2017-present Strumenta and contributors, licensed under Apache 2.0.
 // Copyright 2024-present Strumenta and contributors, licensed under BSD 3-Clause.
-
 package com.strumenta.antlrkotlin.runtime
 
 // TODO(Edoardo): implement real weak keys.
 //  See kotlinlang.org/api/latest/jvm/stdlib/kotlin.native.ref
 //  for classes and functions useful for a possible implementation
-public actual typealias WeakHashMap<K, V> = HashMap<K, V>
+@Suppress("ACTUAL_WITHOUT_EXPECT")
+internal actual typealias WeakHashMap<K, V> = HashMap<K, V>
