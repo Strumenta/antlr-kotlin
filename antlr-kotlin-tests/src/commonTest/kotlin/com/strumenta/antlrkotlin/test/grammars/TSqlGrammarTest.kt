c2ff27ded79b9adb6292627fb93a8977be3a2a20
Edoardo Luppi <lp.edoardo@gmail.com>
2025-06-08 20:16:03 +0200

    test: move TSqlGrammarTest back to commonTest

--- antlr-kotlin-tests/src/commonTest/kotlin/com/strumenta/antlrkotlin/test/grammars/TSqlGrammarTest.kt
+++ antlr-kotlin-tests/src/commonTest/kotlin/com/strumenta/antlrkotlin/test/grammars/TSqlGrammarTest.kt
@@ -11,12 +11,8 @@ import org.antlr.v4.kotlinruntime.CharStream
 import org.antlr.v4.kotlinruntime.ParserRuleContext
 import org.antlr.v4.kotlinruntime.TokenStream
 
-// Note(Edoardo): move the test back to commonTest once
-//  the MethodTooLargeException has been solved on the JVM
-//  by the Kotlin team.
-//  FQN: org.jetbrains.org.objectweb.asm.MethodTooLargeException
 @Suppress("unused")
-class TSqlGrammarNativeTest : GrammarTest<TSqlLexer, TSqlParser>() {
+class TSqlGrammarTest : GrammarTest<TSqlLexer, TSqlParser>() {
   override fun createLexer(input: CharStream): TSqlLexer =
     TSqlLexer(input)
 
