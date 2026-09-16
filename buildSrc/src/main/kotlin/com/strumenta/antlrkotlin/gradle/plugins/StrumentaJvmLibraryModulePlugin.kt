e81f8201a27335fa0cf848a99a5228f65d243a1d
Edoardo Luppi <lp.edoardo@gmail.com>
2026-09-16 09:59:43 +0200

    build: extract the JVM and Java versions to shared constants

--- buildSrc/src/main/kotlin/com/strumenta/antlrkotlin/gradle/plugins/StrumentaJvmLibraryModulePlugin.kt
+++ buildSrc/src/main/kotlin/com/strumenta/antlrkotlin/gradle/plugins/StrumentaJvmLibraryModulePlugin.kt
@@ -2,12 +2,10 @@ package com.strumenta.antlrkotlin.gradle.plugins
 
 import com.strumenta.antlrkotlin.gradle.ext.javaExtension
 import com.strumenta.antlrkotlin.gradle.ext.kotlinJvmExtension
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
 /**
@@ -25,12 +23,12 @@ class StrumentaJvmLibraryModulePlugin : Plugin<Project> {
       apiVersion.set(KOTLIN_VERSION)
       languageVersion.set(KOTLIN_VERSION)
 
-      jvmTarget.set(JvmTarget.JVM_1_8)
+      jvmTarget.set(JVM_VERSION)
       jvmDefault.set(JvmDefaultMode.NO_COMPATIBILITY)
     }
 
     val java = project.javaExtension
-    java.sourceCompatibility = JavaVersion.VERSION_1_8
-    java.targetCompatibility = JavaVersion.VERSION_1_8
+    java.sourceCompatibility = JAVA_VERSION
+    java.targetCompatibility = JAVA_VERSION
   }
 }
--- buildSrc/src/main/kotlin/com/strumenta/antlrkotlin/gradle/plugins/StrumentaVersions.kt
+++ buildSrc/src/main/kotlin/com/strumenta/antlrkotlin/gradle/plugins/StrumentaVersions.kt
@@ -1,8 +1,40 @@
 package com.strumenta.antlrkotlin.gradle.plugins
 
+import org.gradle.api.JavaVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 /**
  * The target Kotlin API and Language version for all antlr-kotlin modules.
  */
 val KOTLIN_VERSION = KotlinVersion.KOTLIN_2_4
+
+/**
+ * The target bytecode version for the JVM compilation of all antlr-kotlin modules.
+ */
+val JVM_VERSION = JvmTarget.JVM_1_8
+
+/**
+ * The target Java version corresponding to the target [JVM_VERSION].
+ */
+val JAVA_VERSION = when (JVM_VERSION) {
+  JvmTarget.JVM_1_8 -> JavaVersion.VERSION_1_8
+  JvmTarget.JVM_9 -> JavaVersion.VERSION_1_9
+  JvmTarget.JVM_10 -> JavaVersion.VERSION_1_10
+  JvmTarget.JVM_11 -> JavaVersion.VERSION_11
+  JvmTarget.JVM_12 -> JavaVersion.VERSION_12
+  JvmTarget.JVM_13 -> JavaVersion.VERSION_13
+  JvmTarget.JVM_14 -> JavaVersion.VERSION_14
+  JvmTarget.JVM_15 -> JavaVersion.VERSION_15
+  JvmTarget.JVM_16 -> JavaVersion.VERSION_16
+  JvmTarget.JVM_17 -> JavaVersion.VERSION_17
+  JvmTarget.JVM_18 -> JavaVersion.VERSION_18
+  JvmTarget.JVM_19 -> JavaVersion.VERSION_19
+  JvmTarget.JVM_20 -> JavaVersion.VERSION_20
+  JvmTarget.JVM_21 -> JavaVersion.VERSION_21
+  JvmTarget.JVM_22 -> JavaVersion.VERSION_22
+  JvmTarget.JVM_23 -> JavaVersion.VERSION_23
+  JvmTarget.JVM_24 -> JavaVersion.VERSION_24
+  JvmTarget.JVM_25 -> JavaVersion.VERSION_25
+  JvmTarget.JVM_26 -> JavaVersion.VERSION_26
+}
